d0964434a6ef374ce61e433a082866ecc3739413
yuan <yuan.li@live.com>
2021-04-06 12:38:34 +0000

    fix terraform cloud connection

--- provider.tf
+++ provider.tf
@@ -2,3 +2,29 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
+  backend "remote" {
+    organization = "beanxyz"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+
+
+
